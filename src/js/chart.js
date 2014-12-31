2956db185e067272048591fd50f28c41fbbdc996
kgryte <kgryte@gmail.com>
2014-12-31 10:32:47 -0800

    [UPDATE] basic config change stub.

--- src/js/chart.js
+++ src/js/chart.js
@@ -1588,29 +1588,29 @@ Chart.prototype.configChanged = function( oldConfig, newConfig ) {
 	bool = this.autoUpdate;
 	this.autoUpdate = false;
 
-	this.width = newConfig.canvas.width;
-	this.height = newConfig.canvas.height;
+	// this.width = newConfig.canvas.width;
+	// this.height = newConfig.canvas.height;
 
 	// FIXME: title should not be part of annotations, but meta. The config should be standardized. Put in repo. Version it. Create an associated validator. NPM.
-	this.chartTitle = newConfig.annotations.title;
-	this.xLabel = newConfig.axes[ 0 ].label;
-	this.yLabel = newConfig.axes[ 1 ].label;
-	this.xMin = newConfig.scales[ 0 ].domain[ 0 ];
-	this.xMax = newConfig.scales[ 0 ].domain[ 1 ];
-	this.yMin = newConfig.scales[ 1 ].domain[ 0 ];
-	this.yMax = newConfig.scales[ 1 ].domain[ 1 ];
-	this.xNumTicks = newConfig.axes[ 0 ].ticks;
-	this.yNumTicks = newConfig.axes[ 1 ].ticks;
-	this.xTickFormat = newConfig.axes[ 0 ].format;
-	this.xAxisOrient = newConfig.axes[ 0 ].orient;
-	this.yAxisOrient = newConfig.axes[ 1 ].orient;
+	// this.chartTitle = newConfig.annotations.title;
+	// this.xLabel = newConfig.axes[ 0 ].label;
+	// this.yLabel = newConfig.axes[ 1 ].label;
+	// this.xMin = newConfig.scales[ 0 ].domain[ 0 ];
+	// this.xMax = newConfig.scales[ 0 ].domain[ 1 ];
+	// this.yMin = newConfig.scales[ 1 ].domain[ 0 ];
+	// this.yMax = newConfig.scales[ 1 ].domain[ 1 ];
+	// this.xNumTicks = newConfig.axes[ 0 ].ticks;
+	// this.yNumTicks = newConfig.axes[ 1 ].ticks;
+	// this.xTickFormat = newConfig.axes[ 0 ].format;
+	// this.xAxisOrient = newConfig.axes[ 0 ].orient;
+	// this.yAxisOrient = newConfig.axes[ 1 ].orient;
 
 	// TODO: support this and `tension` for multiple lines.
 	// this.interpolation = newConfig.marks[ 0 ].properties.interpolation;
 
-	this.labels = newConfig.marks.map( function onMark( mark ) {
-		return mark.data;
-	});
+	// this.labels = newConfig.marks.map( function onMark( mark ) {
+	// 	return mark.data;
+	// });
 
 	this.fire( 'changed', {
 		'attr': 'config',
@@ -1620,6 +1620,11 @@ Chart.prototype.configChanged = function( oldConfig, newConfig ) {
 
 	// Reset the auto update flag to its original value:
 	this.autoUpdate = bool;
+
+	// Only if auto update is enabled, redraw the chart...
+	if ( bool ) {
+		this.create();
+	}
 }; // end METHOD configChanged()
 
 /**
