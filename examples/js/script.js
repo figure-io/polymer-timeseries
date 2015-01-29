51ad17e9071aac7ab239a9ea3c71bcb72f21804e
kgryte <kgryte@gmail.com>
2015-01-28 20:13:37 -0800

    [EXAMPLES] polymer ready event listener.

--- examples/js/script.js
+++ examples/js/script.js
@@ -76,9 +76,8 @@
 	// SCRIPT //
 
 	// Get the template for a figure configuration:
-	// FIXME: addresses differences in lifecycle behavior between Chrome and Firefox.
-	setTimeout( function() {
+	window.addEventListener( 'polymer-ready', function onReady() {
 		getResource( './../examples/data/timeseries.json', onData );
-	}, 500 );
+	});
 
 })();
